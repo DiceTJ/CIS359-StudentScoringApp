53f0887740d61338ee53c4dd93f8547d43f26795
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-09-28 20:51:32 -0600

    Student Scores Design Completed

--- CIS359-StudentScoringApp/frmStudentScores.cs
+++ CIS359-StudentScoringApp/frmStudentScores.cs
@@ -16,5 +16,15 @@ namespace CIS359_StudentScoringApp
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
