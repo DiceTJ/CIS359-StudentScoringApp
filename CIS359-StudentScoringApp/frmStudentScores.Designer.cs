bb85cfc7a89c6574fdddddf23c128f3577238e92
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-09-28 20:26:49 -0600

    Add project files.

---
+++ CIS359-StudentScoringApp/frmStudentScores.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace CIS359_StudentScoringApp
+{
+    partial class frmStudentScores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmStudentScores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 208);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmStudentScores";
+            this.Text = "Student Scores";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
