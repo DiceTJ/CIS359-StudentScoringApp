3e5c384f5d426ac5c720ee4ad36d3523acc7a305
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-11-09 21:01:29 -0700

    updated addupdatescore

--- CIS359-StudentScoringApp/frmAddUpdateScore.Designer.cs
+++ CIS359-StudentScoringApp/frmAddUpdateScore.Designer.cs
@@ -44,6 +44,7 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Score:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtScore
             // 
--- CIS359-StudentScoringApp/frmAddUpdateScore.cs
+++ CIS359-StudentScoringApp/frmAddUpdateScore.cs
@@ -32,9 +32,9 @@ namespace CIS359_StudentScoringApp
             bool success = true;
 
             StringBuilder sb = new StringBuilder();
-            sb.Append(IsPresent(txtScore.Text, "Score"));
-            sb.Append(IsInt32(txtScore.Text, "Score"));
-            sb.Append(IsWithinRange(txtScore.Text, "Score", 0, 100));
+            sb.Append(Validator.IsPresent(txtScore.Text, "Score"));
+            sb.Append(Validator.IsInt32(txtScore.Text, "Score"));
+            sb.Append(Validator.IsWithinRange(txtScore.Text, "Score", 0, 100));
             string errorMsg = sb.ToString();
 
             if (!String.IsNullOrEmpty(errorMsg))
@@ -46,39 +46,7 @@ namespace CIS359_StudentScoringApp
             return success;
         }
 
-        private string IsPresent(string value, string name)
-        {
-            string errorMsg = "";
-            if (String.IsNullOrEmpty(value))
-            {
-                errorMsg = $"{name} is a required field.\n";
-            }
-            return errorMsg;
-        }
-
-        private string IsInt32(string value, string name)
-        {
-            string errorMsg = "";
-            if (!Int32.TryParse(value, out _))
-            {
-                errorMsg = $"{name} is a required field.\n";
-            }
-            return errorMsg;
-        }
-
-        private string IsWithinRange(string value, string name, decimal min, decimal max)
-        {
-            string errorMsg = "";
-
-            if (Decimal.TryParse(value, out decimal number))
-            {
-                if (number < min || number > max)
-                {
-                    errorMsg = $"{name} must be between {min} and {max}.\n";
-                }
-            }
-            return errorMsg;
-        }
+        
 
         private void btnAddUpdate_Click(object sender, EventArgs e)
         {
@@ -92,6 +60,11 @@ namespace CIS359_StudentScoringApp
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
