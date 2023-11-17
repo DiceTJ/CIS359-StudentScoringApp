1eec35ac76b2a55aac839befdf1a7254ec185371
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-11-16 19:40:14 -0700

    Added student db class

---
+++ CIS359-StudentScoringApp/StudentDB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CIS359_StudentScoringApp
+{
+    public class StudentDB
+    {
+        private const string dir = @"C:\C#\Files\";
+        private const string path = dir + "StudentScores.txt";
+
+
+        public static List<Student> GetStudents()
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+            StreamReader textIn = new StreamReader(Path.Combine(path));
+            
+            List<Student> students = new List<Student>();
+
+            while (textIn.Peek() != -1)
+            {
+                string row = textIn.ReadLine() ?? "";
+                Student student = new Student(row);
+                students.Add(student);
+            }
+            
+            
+            return students;
+        }
+
+        public static void SaveStudents(List<Student> students)
+        {
+            StreamWriter textOut = new StreamWriter(path, true);
+        
+            foreach (Student student in students)
+                textOut.WriteLine(student.ToString());
+        }
+
+    }
+}
